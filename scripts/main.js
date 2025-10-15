86e0b8327317470ecbece9369fba7b0b2395a990
fredgui10 <freddy_flores1985@outlook.com>
2025-10-16 01:16:35 +0200

    updated the JS

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("register-form");
   if (form) {
     form.addEventListener("submit", () => {
-      alert("¡Gracias por registrarte! 🎄 Pronto recibirás la invitación por WhatsApp.");
+      alert("¡Gracias por registrarte!");
     });
   }
 });
